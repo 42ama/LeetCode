81e96d41434845b3aae704044e70caf6b8fcd5c4
Maxim <amatuer42@gmail.com>
2023-08-29 14:16:00 +0400

    Добавил решение по ValidParentheses

---
+++ NeetCodeRoadmap/Stack/ValidParenthesesSolution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeetCodeRoadmap.Stack
+{
+    internal class ValidParenthesesSolution
+    {
+        public bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+
+            var leftElemnts = new HashSet<char> { '(', '[', '{' };
+            var corresponidng = new Dictionary<char, char>
+            {
+                {')', '(' },
+                {']', '[' },
+                {'}', '{' }
+            };
+
+            foreach (char symbol in s)
+            {
+                if (leftElemnts.Contains(symbol))
+                {
+                    stack.Push(symbol);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    var stackedElemnt = stack.Pop();
+                    if (corresponidng[symbol] != stackedElemnt)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
