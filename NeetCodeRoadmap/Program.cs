1a2ec71a7a0257ccda77e57fb71a5f32c52b67a0
Maxim <amatuer42@gmail.com>
2023-09-12 17:24:38 +0400

    Добавил решение для DailyTemperatures

--- NeetCodeRoadmap/Program.cs
+++ NeetCodeRoadmap/Program.cs
@@ -1,1 +1,5 @@
 ﻿
+using NeetCodeRoadmap.Stack;
+
+var solution = new DailyTemperaturesSolution();
+solution.DailyTemperatures(new int[] { 93, 97, 32, 43, 78 });
---
+++ NeetCodeRoadmap/Stack/DailyTemperaturesSolution.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeetCodeRoadmap.Stack
+{
+    internal class DailyTemperaturesSolution
+    {
+        /// <summary>
+        /// Набираем стек пока не найдём температуру больше текущей, после проверяем все элементы из стека. Для тех которые меньше устанавливаем дальность в ответ, остальные оставляем в стеке. После основного прохода проходимся по стеку - дочищаем элементы которым не нашлось более теплого дня.
+        /// </summary>
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            var result = new int[temperatures.Length];
+
+            var stack = new Stack<(int, int)>();
+            stack.Push((temperatures[0], 0));
+
+            for (int i = 1; i < result.Length; i++)
+            {
+                var current = temperatures[i];
+                var previous = temperatures[i - 1];
+                if (current > previous)
+                {
+                    while (stack.Count > 0)
+                    {
+                        var stackPrevious = stack.Peek();
+
+                        if (current > stackPrevious.Item1)
+                        {
+                            result[stackPrevious.Item2] = i - stackPrevious.Item2;
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                stack.Push((current, i));
+            }
+
+            foreach (var item in stack)
+            {
+                result[item.Item2] = 0;
+            }
+
+            return result;
+        }
+    }
+}
