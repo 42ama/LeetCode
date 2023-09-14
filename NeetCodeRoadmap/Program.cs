00ab6ea3ea60aaabf30fa1dfc2fef4132ee92ab9
Maxim <amatuer42@gmail.com>
2023-09-14 14:44:59 +0400

    Накидал основу для решения CarFleetSolution (причём тут стэк?)

--- NeetCodeRoadmap/Program.cs
+++ NeetCodeRoadmap/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using NeetCodeRoadmap.Stack;
 
-var solution = new DailyTemperaturesSolution();
-solution.DailyTemperatures(new int[] { 93, 97, 32, 43, 78 });
+var solution = new CarFleetSolution();
+var result = solution.CarFleet(10, new int[] { 8, 3, 7, 4, 6, 5 }, new int[] { 4, 4, 4, 4, 4, 4 });
+Console.WriteLine(result); // output: 2, expected: 6
---
+++ NeetCodeRoadmap/Stack/CarFleetSolution.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeetCodeRoadmap.Stack
+{
+    internal class CarFleetSolution
+    {
+        public int CarFleet(int target, int[] position, int[] speed)
+        {
+            var numberOfCars = position.Length;
+
+            var fleetCount = 0;
+            while (true)
+            {
+                var reachNMileThisIteration = new Dictionary<int, List<int>>();
+                var processedCarCounter = 0;
+
+                for (int currentCarIndex = 0; currentCarIndex < numberOfCars; currentCarIndex++)
+                {
+                    if (position[currentCarIndex] == target)
+                    {
+                        processedCarCounter++;
+                        if (processedCarCounter == numberOfCars)
+                        {
+                            return fleetCount;
+                        }
+                        else
+                        {
+                            continue;
+                        }                        
+                    }
+
+                    position[currentCarIndex] = Math.Clamp(position[currentCarIndex] + speed[currentCarIndex], 0, target);
+
+                    if (reachNMileThisIteration.TryGetValue(position[currentCarIndex], out var cars))
+                    {
+                        if (position[currentCarIndex] != target)
+                        {
+                            foreach (var otherCarIndex in cars)
+                            {
+                                if (speed[currentCarIndex] > speed[otherCarIndex])
+                                {
+                                    speed[currentCarIndex] = speed[otherCarIndex];
+                                }
+                                else
+                                {
+                                    speed[otherCarIndex] = speed[currentCarIndex];
+                                }
+                            }
+                        }
+
+                        cars.Add(currentCarIndex);
+                    }
+                    else
+                    {
+                        reachNMileThisIteration.Add(position[currentCarIndex], new List<int> { currentCarIndex });
+                    }
+                }
+
+                fleetCount += reachNMileThisIteration.Count(i => i.Key == target);
+            }
+        }
+    }
+}
