82b52f275f3f90f23d82000130f4b2e0cf8d9a18
Maxim <amatuer42@gmail.com>
2023-09-24 09:54:36 +0400

    TwoSumIISolution

--- NeetCodeRoadmap/Program.cs
+++ NeetCodeRoadmap/Program.cs
@@ -2,6 +2,6 @@
 using NeetCodeRoadmap.Stack;
 using NeetCodeRoadmap.TwoPointers;
 
-var solution = new ValidPalindromeSolution();
-var result = solution.IsPalindrome(",,,,,,,,,,,,acva");
+var solution = new TwoSumIISolution();
+var result = solution.TwoSum(new int[] { 1, 3, 5, 6, 7, }, 9);
 Console.WriteLine(result); 
---
+++ NeetCodeRoadmap/TwoPointers/TwoSumIISolution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeetCodeRoadmap.TwoPointers
+{
+    internal class TwoSumIISolution
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            var leftPointer = 0;
+            var rightPointer = numbers.Length - 1;
+
+            var leftNumber = numbers[leftPointer];
+            var rightNumber = numbers[rightPointer];
+
+            while (true)
+            {
+                if (leftNumber + rightNumber == target)
+                {
+                    return new int[] { leftPointer + 1, rightPointer + 1 };
+                }
+
+                while (leftNumber + rightNumber > target)
+                {
+                    rightPointer--;
+                    rightNumber = numbers[rightPointer];
+                }
+
+                while (leftNumber + rightNumber < target)
+                {
+                    leftPointer++;
+                    leftNumber = numbers[leftPointer];
+                }
+            }
+
+            throw new Exception();
+        }
+    }
+}
